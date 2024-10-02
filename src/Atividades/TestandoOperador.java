f01e512c4d7715271acd6eea76677b6f4a687d88
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2024-10-02 10:04:12 -0300

    update de atividades na sala de aula

---
+++ src/Atividades/TestandoOperador.java
@@ -0,0 +1,9 @@
+package Atividades;
+
+public class TestandoOperador {
+    public static void main(String[] args){
+        int a=5, b=5;
+        System.out.println(a++);
+        System.out.println(a--);
+    }
+}
