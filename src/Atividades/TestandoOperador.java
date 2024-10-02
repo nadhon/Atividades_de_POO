ea66e86dfaa5c7af8be28f1cf0fd1cda20746702
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2024-10-02 10:09:27 -0300

    Update TestandoOperador.java

--- src/Atividades/TestandoOperador.java
+++ src/Atividades/TestandoOperador.java
@@ -3,7 +3,7 @@ package Atividades;
 public class TestandoOperador {
     public static void main(String[] args){
         int a=5, b=5;
-        System.out.println(a++);
-        System.out.println(a--);
+        System.out.println(++a);
+        System.out.println(b++);
     }
 }
