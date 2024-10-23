fc52339e43c6161eeb963c4c50d53bbe3a73f461
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2024-10-23 11:10:51 -0300

    Update Main.java

--- src/Atividades/Main.java
+++ src/Atividades/Main.java
@@ -2,9 +2,9 @@ package Atividades;
 
 public class Main {
     public static void main(String[] args){
-        RepositorioContas repositorio =new RepoistorioConta();
         Cliente cliente = new Cliente("maria","045.139.552-83");
         Conta c =  new Conta("27.192-3",580.73,cliente);
         Poupanca p = new Poupanca("21.342-7",400.50,0.01,cliente);
+        System.out.println(c);
     }
 }
