4ae75737e981ee6168af973adb4d9183f4180399
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2024-11-04 10:02:10 -0300

    update separando os corres para a criação desse aplicativo

---
+++ src/Banco/Cliente.java
@@ -0,0 +1,22 @@
+package Banco;
+
+public class Cliente {
+    public String nome;
+    public String cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
--- src/Banco/Conta.java
+++ src/Banco/Conta.java
@@ -2,5 +2,6 @@ package Banco;
 
 public class Conta {
     private int numero;
-    
+    Cliente cliente;
+    public double saldo;
 }
