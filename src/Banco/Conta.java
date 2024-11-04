ee6485e2d8b439a4be7f961bdb80a3e0db942101
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2024-11-04 10:13:12 -0300

    Update Conta.java

--- src/Banco/Conta.java
+++ src/Banco/Conta.java
@@ -4,4 +4,31 @@ public class Conta {
     private int numero;
     Cliente cliente;
     public double saldo;
+
+    public Conta(String c,double s,Cliente cl){
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
