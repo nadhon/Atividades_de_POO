f06adf199192e5b5ca42792eae2d26a2cbfa3375
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2024-11-25 10:55:26 -0300

    o gato mia e o cachorro late e eu choro

--- src/Selva/Cachorro.java
+++ src/Selva/Cachorro.java
@@ -3,6 +3,7 @@ package Selva;
 public class Cachorro  extends Animal{
     @Override
     void falar(){
+        System.out.println("Cachorro");
         System.out.println("Latir");
     }
 }
--- src/Selva/Gato.java
+++ src/Selva/Gato.java
@@ -3,6 +3,7 @@ package Selva;
 public class Gato  extends Animal{
     @Override
     void falar(){
+        System.out.println("Gato");
         System.out.println("miar");
     }
 }
