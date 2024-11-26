412897c065e9cd7a0d42af971c5e5043982825ec
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2024-11-26 19:29:54 -0300

    atividade de POO

---
+++ src/Forma/Figura.java
@@ -0,0 +1,13 @@
+package Forma;
+
+public class Figura {
+    public int area;
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+}
---
+++ src/Forma/Quadrado.java
@@ -0,0 +1,4 @@
+package Forma;
+
+public class Quadrado extends Figura{
+}
