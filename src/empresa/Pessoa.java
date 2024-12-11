a6818096681510eb8782b5ea21c4b5d57ed0072c
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2024-12-11 09:55:07 -0300

    projeto

---
+++ src/empresa/Pessoa.java
@@ -0,0 +1,33 @@
+package empresa;
+
+public abstract class Pessoa {
+    public String nome;
+    public String cpf;
+    public double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    abstract double bonificacao();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
