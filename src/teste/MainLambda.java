f28e1a8703e41d7cef6b40669ce2d7b858bef8bf
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2025-04-24 18:13:02 -0300

    problema

--- src/teste/MainLambda.java
+++ src/teste/MainLambda.java
@@ -1,12 +1,18 @@
 package teste;
 
+import java.util.Arrays;
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
 public class MainLambda {
     public static void main(String[] args){
-		System.out.println(somar(10, 15));
-		System.out.println(somar(187, 18));
+        System.out.println(resultado);
 
-		System.out.println(fatorial(5));
-		System.out.println(fatorial(7));
+		int numero= 5;
+        int numero2=7;
+        System.out.println(numero+"!= "+fatorial(numero));
+        System.out.println(numero2+"!= "+fatorial(numero2));
 
 		System.out.println(isPrimo(13));
 		System.out.println(isPrimo(14));
@@ -24,16 +30,20 @@ public class MainLambda {
     imprimirMaiorNumero(array2);
 }
     public static boolean isPrimo(int numero) {
-        int divisiveis = 0;
-
-        for (int i = 1; i <= numero; i++) {
-            if (numero % i == 0) {
-                divisiveis++;
-            }
-        }
-
-        return divisiveis == 2;
+        if(numero <=1)return false;
+        return IntStream.rangeClosed(2,(int)Math.sqrt(numero))
+                .noneMatch(i ->numero%i ==0);
     }
+//        int divisiveis = 0;
+//
+//        for (int i = 1; i <= numero; i++) {
+//            if (numero % i == 0) {
+//                divisiveis++;
+//            }
+//        }
+//
+//        return divisiveis == 2;
+//    }
     public static boolean isInteiroValido(String texto) {
         return texto.matches("-?\\d+");
     }
@@ -51,36 +61,44 @@ public class MainLambda {
 
         return soma / vetor.length;
     }
-
-public static int somar(int n1, int n2) {
-    return n1 + n2;
-}
-
-public static int fatorial(int numero) {
-    if (numero < 0) {
-        throw new IllegalArgumentException("numero < 0");
+    @FunctionalInterface
+    public interface FunctionInter{
+        int calcular(int n1, int n2);
     }
+    static FunctionInter somar = (n1, n2)-> n1+n2;
+    static int resultado = somar.calcular(15,10);
+//public static int somar(int n1, int n2) {
+//    return n1 + n2;
+//}
 
-    int resultado = 1;
-
-    for (int i = 1; i <= numero; i++) {
-        resultado *= i;
-    }
-
-    return resultado;
+public static long fatorial(int numero) {
+    return LongStream.rangeClosed(1,numero)
+            .reduce(1,(a,b)->a*b);
 }
+//    if (numero < 0) {
+//        throw new IllegalArgumentException("numero < 0");
+//    }
+//
+//    int resultado = 1;
+//
+//    for (int i = 1; i <= numero; i++) {
+//        resultado *= i;
+//    }
+//
+//    return resultado;
+//}
     public static void imprimirMaiorNumero(double[] vetor) {
         if (vetor.length == 0) {
             throw new IllegalArgumentException("vetor.length == 0");
         }
 
-        double maiorNumero = vetor[0];
+        OptionalDouble maiorNumero = Arrays.stream(vetor).max();
 
-        for (int i = 1; i < vetor.length; i++) {
-            if (vetor[i] > maiorNumero) {
-                maiorNumero = vetor[i];
-            }
-        }
+//        for (int i = 1; i < vetor.length; i++) {
+//            if (vetor[i] > maiorNumero) {
+//                maiorNumero = vetor[i];
+//            }
+//        }
 
         System.out.println("Maior número = " + maiorNumero);
     }
